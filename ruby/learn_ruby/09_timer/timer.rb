803064d29e554e6c840bd7fced85ad5e8169ecdf
Scott Yu <scott.tj.yu@gmail.com>
2014-12-09 15:59:41 +0800

    solved timer

---
+++ ruby/learn_ruby/09_timer/timer.rb
@@ -0,0 +1,29 @@
+class Timer
+
+def initialize
+  @seconds = 0
+end
+
+def seconds=(num)
+  @seconds = num
+end
+
+def seconds
+  @seconds
+end
+
+def time_string
+  hour    = (@seconds/3600)
+  minutes = (@seconds/60)%60
+  seconds = (@seconds%60)
+
+  padded(hour).to_s + ":" + padded(minutes).to_s + ":" + padded(seconds).to_s
+end
+
+def padded(num)
+  num < 10 ? "0" + num.to_s : num.to_s
+end
+
+
+
+end
