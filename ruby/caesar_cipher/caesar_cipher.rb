03b321444fe313c9518131ffabb45332752594d1
Scott Yu <scott.tj.yu@gmail.com>
2014-12-10 16:57:23 +0800

    added loop test case

--- ruby/caesar_cipher/caesar_cipher.rb
+++ ruby/caesar_cipher/caesar_cipher.rb
@@ -3,21 +3,20 @@
 #uppercase characters A-Z(#65-90), lowercase characters a-z(#97-#122).
 
 def caesar_cipher(phrase, shift)
+ 
+  #take remainder in a 26 character cycle
+  shift = shift % 26
 
-#take remainder in a 26 character cycle
-shift = shift % 26
+  #since we'll using ASCII as the base for the shift, we'll treat lowercase and uppercase characters separately. 
+  alphabet_lc = ("a".."z").to_a
+  alphabet_uc = ("A".."Z").to_a 
 
-#since we'll using ASCII as the base for the shift, we'll treat lowercase and uppercase characters separately. 
-alphabet_lc = ("a".."z").to_a
-alphabet_uc = ("A".."Z").to_a 
+  #takes a phrase lowercase then calculate its ascii value excluding special characters
+  #use .ord and .chr methods to convert between ASCII and Character
+  lc_converted = phrase.chars.map {|c| alphabet_lc.include?(c) ? (lc_loop(c.ord + shift).chr) : c }
 
-#takes a phrase lowercase then calculate its ascii value excluding special characters
-#use .ord and .chr methods to convert between ASCII and Character
-lc_converted = phrase.chars.map {|c| alphabet_lc.include?(c) ? (lc_loop(c.ord + shift).chr) : c }
-
-
-convert_capital = lc_converted.map {|c| alphabet_uc.include?(c) ? (uc_loop(c.ord + shift).chr) : c }.join("")
-
+  convert_capital = lc_converted.map {|c| alphabet_uc.include?(c) ? (uc_loop(c.ord + shift).chr) : c }.join("")
+end
 
 #Note that we have below two methods to loop back from a/A if the shift pushes the characters beyond z/Z. 
 
@@ -27,8 +26,6 @@ def uc_loop(n)
 end
 
 #a-z (97-122)
-end
-
 def lc_loop(n)
   n > 122 ? n % 122 + 96 : n
 end
--- ruby/caesar_cipher/caesar_cipher_spec.rb
+++ ruby/caesar_cipher/caesar_cipher_spec.rb
@@ -16,6 +16,10 @@ describe 'Caesar Cipher' do
     caesar_cipher("HeLLo", 2) == "JeNNq"
   end
 
+  it "characters that shift beyond z/Z should loop back around to a/A" do
+    caesar_cipher("xYZz", 5) == "cDEe"
+  end
+
   it "should complete assignment sample test case" do
     caesar_cipher("What a string!", 5) == "Bmfy f xywnsl!"
   end
